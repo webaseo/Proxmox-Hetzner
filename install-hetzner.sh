6ae1855c122555d5ae81bc97d4d223440d695f1e
Andreas Witt <55405893+andibacken234@users.noreply.github.com>
2020-05-16 17:18:59 +0200

    Update install-hetzner.sh

--- install-hetzner.sh
+++ install-hetzner.sh
@@ -245,7 +245,7 @@ fi
 curl "https://raw.githubusercontent.com/hetzneronline/installimage/master/post-install/proxmox6" --output /post-install
 
 #Customising post install file
-echo "wget https://raw.githubusercontent.com/extremeshok/xshok-proxmox/master/install-post.sh -c -O install-post.sh && bash install-post.sh && rm install-post.sh" >> /post-install
+echo "wget https://raw.githubusercontent.com/andibacken234/Proxmox-Hetzner/master/install-post.sh -c -O install-post.sh && bash install-post.sh && rm install-post.sh" >> /post-install
 
 if grep -q '#!/bin/bash' "/post-install"; then
   chmod 777 /post-install
